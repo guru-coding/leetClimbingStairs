b57d931d368a1b25ab12b3831822e0fab81ac485
Ruslan Gusmanov <gusmanovruslan@gmail.com>
2023-12-22 16:13:09 +0300

    Skip constraint check because N is declared greater than or equal 1
    LeetCode constraints:
    * 1 <= n <= 45

--- leetClimbingStairs.go
+++ leetClimbingStairs.go
@@ -2,11 +2,9 @@ package leetClimbingStairs
 
 func climbStairs(n int) int {
 	dp := make([]int, n+1)
-	var i int
-	for i = 0; i <= 1 && i <= n; i++ {
-		dp[i] = 1
-	}
-	for i = 2; i <= n; i++ {
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i <= n; i++ {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
 	return dp[n]
